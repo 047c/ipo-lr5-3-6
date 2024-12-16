3d3623965dea52e28c937f81e5d744cc3f98f962
047c <maks.smirnov.smolensk@gmail.com>
2024-12-16 19:53:23 +0300

    renaming

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 with open("text.txt", 'r') as file:  # Открываем файл, выдав имя file
-    stroka = file.read()  # Читаем строку
-slova = stroka.split()  # Делим строку на слова, записывая в список
+    text = file.read()  # Читаем строку
+words = text.split()  # Делим строку на слова, записывая в список
 unicum = list()  # Создаем новый список для уникальных слов
-for slovo in slova:  # Для слова в словах
-    if slovo in unicum:  # Если слово в списке для уникальных слов
-        pass  # Ничего не делаем
-    else:  # Или
-        unicum.append(slovo)  # Записываем слово в список с уникальными словами
+for word in words:  # Для слова в словах
+    if word not in unicum:  # Если слово не в списке для уникальных слов
+        unicum.append(word) # Записываем слово в список с уникальными словами
 with open("output.txt", "w") as file:  # Открыв файл, даём ему имя file
     file.write("Список уникальных слов из text.txt: ")  # Записываем в файл текст
-    for slovo in unicum:  # Для каждого слова в уникальном списке
-        file.write(slovo + " ")  # Записать это слово в файл
+    for word in unicum:  # Для каждого слова в уникальном списке
+        file.write(word + " ")  # Записать это слово в файл
